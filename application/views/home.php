b1a2631693fdca612997594368a3eb668dca8d80
ystmrdk <rattyasta1@gmail.com>
2019-02-02 11:07:59 +0700

    tambahan

--- application/views/home.php
+++ application/views/home.php
@@ -152,125 +152,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
              </div> -->
          </div>
          <!-- Start home section -->
-         <div id="home">
-            <!-- Start cSlider -->
-            <div id="da-slider" class="da-slider">
-                <div class="triangle"></div>
-                <!-- mask elemet use for masking background image -->
-                <div class="mask"></div>
-                <!-- All slides centred in container element -->
-                <div class="container">
-                    <!-- Start first slide -->
-                    <div class="da-slide">
-                        <h2 class="fittext2">Si Djaka</h2>
-                        <h4>Clean & responsive</h4>
-                        <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
-                        <a href="#" class="da-link button">Readmore</a>
-                        <div class="da-img">
-                            <img src="<?php echo base_url() ?>assets/images/bunderan/IconSijaka2.png" alt="image01" width="320" background-color="white">
-                        </div>
-                    </div>
-                    <!-- End first slide -->
-                    <!-- Start second slide -->
-                    <div class="da-slide">
-                        <h2>E-uji</h2>
-                        <h4>Easy to use</h4>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                        <a href="#" class="da-link button">Read more</a>
-                        <div class="da-img">
-                            <img src="<?php echo base_url() ?>assets/images/Slider02.png" width="320" alt="image02">
-                        </div>
-                    </div>
-                    <!-- End second slide -->
-                    <!-- Start third slide -->
-                    <div class="da-slide">
-                        <h2>Info Lalin</h2>
-                        <h4>Customizable</h4>
-                        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-                        <a href="#" class="da-link button">Read more</a>
-                        <div class="da-img">
-                            <img src="<?php echo base_url() ?>assets/images/Slider03.png" width="320" alt="image03">
-                        </div>
-                    </div>
-                    <!-- End third slide -->
-                    <div class="da-slide">
-                        <h2 class="fittext2">Jaladara Werkudara</h2>
-                        <h4>Clean & responsive</h4>
-                        <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
-                        <a href="#" class="da-link button">Read more</a>
-                        <div class="da-img">
-                            <img src="<?php echo base_url() ?>assets/images/Slider01.png" alt="image01" width="320">
-                        </div>
-                    </div>
-                    <div class="da-slide">
-                        <h2 class="fittext2">BST</h2>
-                        <h4>Clean & responsive</h4>
-                        <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
-                        <a href="#" class="da-link button">Read more</a>
-                        <div class="da-img">
-                            <img src="<?php echo base_url() ?>assets/images/Slider01.png" alt="image01" width="320">
-                        </div>
-                    </div>
-                    <div class="da-slide">
-                        <h2 class="fittext2">Derek Gratis</h2>
-                        <h4>Clean & responsive</h4>
-                        <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
-                        <a href="#" class="da-link button">Read more</a>
-                        <div class="da-img">
-                            <img src="<?php echo base_url() ?>assets/images/Slider01.png" alt="image01" width="320">
-                        </div>
-                    </div>
-                    <!-- Start cSlide navigation arrows -->
-                    <div class="da-arrows">
-                        <span class="da-arrows-prev"></span>
-                        <span class="da-arrows-next"></span>
-                    </div>
-                    <!-- End cSlide navigation arrows -->
-                </div>
-            </div>
-        </div>
         <!-- End home section -->
         <!-- Service section start -->
-        <div class="section primary-section" id="service">
-            <div class="container">
-                <!-- Start title section -->
-                <div class="title">
-                    <h1>What We Do?</h1>
-                    <!-- Section's title goes here -->
-                    <p>Duis mollis placerat quam, eget laoreet tellus tempor eu. Quisque dapibus in purus in dignissim.</p>
-                    <!--Simple description for section goes here. -->
-                </div>
-                <div class="row-fluid">
-                    <div class="span4">
-                        <div class="centered service">
-                            <div class="circle-border zoom-in">
-                                <img class="img-circle" src="<?php echo base_url() ?>assets/images/Service1.png" alt="service 1">
-                            </div>
-                            <h3>Modern Design</h3>
-                            <p>We Create Modern And Clean Theme For Your Business Company.</p>
-                        </div>
-                    </div>
-                    <div class="span4">
-                        <div class="centered service">
-                            <div class="circle-border zoom-in">
-                                <img class="img-circle" src="<?php echo base_url() ?>assets/images/Service2.png" alt="service 2" />
-                            </div>
-                            <h3>Powerfull Theme</h3>
-                            <p>We Create Modern And Powerful Theme With Lots Animation And Features</p>
-                        </div>
-                    </div>
-                    <div class="span4">
-                        <div class="centered service">
-                            <div class="circle-border zoom-in">
-                                <img class="img-circle" src="<?php echo base_url() ?>assets/images/Service3.png" alt="service 3">
-                            </div>
-                            <h3>Clean Code</h3>
-                            <p>We Create Modern And Powerful Html5 And CSS3 Code Easy For Read And Customize.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        z
         <!-- Service section end -->
         <!-- Portfolio section start --> <!--
         <div class="section secondary-section " id="portfolio">
@@ -673,7 +557,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
         <!-- Portfolio section end -->
         <!-- About us section start -->
-        <div class="telp" id="about" style="background-color:grey" >
+        
+        <div class="telp" id="about">
+            <div style="background-color:grey;text-align:center;font-family:Montserrat">
             <div class="container">
                 <div class="title">
                     <h3>Info Seputar Dinas Perhubungan</h3>
@@ -761,6 +647,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         </div>
                     </div>
                 </div></div>
+                <div>
+                <br><br><br>
               <!--  <div class="about-text centered">
                     <h3>About Us</h3>
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
@@ -958,7 +846,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>-->
         <!-- Price section end -->
         <!-- Newsletter section start -->
-        <br><div>
+        <!-- <br><div>
         <div class="telp">
             <div class="section third-section">
                 <div class="container newsletter">
@@ -984,7 +872,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
                 <!-- Newsletter section end -->
                 <!-- Contact section start -->
                 <div id="contact" class="contact">
@@ -1066,7 +954,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                     </div>
                                 </div>
                             </div>
-                            <div class="container">
+                            <!-- <div class="container">
                                 <div class="span9 center contact-info">
                                     <p>123 Fifth Avenue, 12th,Belgrade,SRB 11000</p>
                                     <p class="info-mail">ourstudio@somemail.com</p>
@@ -1110,14 +998,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                         </li>
                                     </ul>
                                 </div>
-                            </div>
+                            </div> -->
                         </div>
                     </div>
                 </div>
                 <!-- Contact section edn -->
                 <!-- Footer section start -->
                 <div class="footer">
-                    <p>&copy; 2013 Theme by <a href="http://www.graphberry.com">GraphBerry</a>, <a href="http://goo.gl/NM84K2">Documentation</a></p>
+                    <p>&copy; 2018 Theme by <a href="">Komsi BB</a>, <a href="">UGM</a></p>
                 </div>
                 <!-- Footer section end -->
                 <!-- ScrollUp button start -->
